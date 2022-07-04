8320414487a8275c4825db11216d90684f4cb1e1
rosanne-a <anderson.rosanne4@gmail.com>
2022-07-04 15:43:25 -0400

    image data added for image slider(Hues, Pour, RD)

---
+++ src/components/PourData.js
@@ -0,0 +1,34 @@
+export const PourData = [
+    {
+        image: 'https://i.imgur.com/qNWXWLa.png',
+        alt: 'Pour Choices Home Page'
+    },
+    {
+        image: 'https://i.imgur.com/E79dtNi.png',
+        alt: 'Pour Choices Cocktail List'
+    },
+    {
+        image: 'https://i.imgur.com/vf9dUTB.png',
+        alt: 'Pour Choices Cocktail Recipie Card'
+    }
+    ,
+    {
+        image: 'https://i.imgur.com/72553Mn.png',
+        alt: 'Pour Choices User Account'
+    }
+    ,
+    {
+        image: 'https://i.imgur.com/DxoBA8L.png',
+        alt: 'Pour Choices Create List'
+    }
+    ,
+    {
+        image: 'https://i.imgur.com/1xubbyt.png',
+        alt: 'Pour Choices About Page'
+    }
+    ,
+    {
+        image: 'https://i.imgur.com/ICyzzTx.png',
+        alt: 'Pour Choices Cocktail Update Page'
+    }
+]
