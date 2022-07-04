f3730644f72dd0d5a0e06b12569efa71fa2bd77a
rosanne-a <anderson.rosanne4@gmail.com>
2022-07-04 15:16:57 -0400

    Image slider file structure

--- src/components/imageslider.js
+++ src/components/imageslider.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Contact(){
+export default function Slider(){
     return(
-        <div className="contact">
+        <div className="slider">
             
         </div>
     )
